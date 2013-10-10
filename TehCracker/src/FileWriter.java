ae8313330344882669e9536fab0aabe201ac342b
qwertykd <qwertykd@AT-HD-132-TS.ad.ufl.edu>
2013-10-10 14:09:27 -0400

    testing

--- TehCracker/src/FileWriter.java
+++ TehCracker/src/FileWriter.java
@@ -1,4 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class FileWriter {
-
+	public static void main(String[] args){
+		List<String> lines = new ArrayList<String>();
+		lines.add("hello");
+		lines.add("there");
+		log(lines);
+		
+	}
+	private static void log(Object aMsg){
+	    System.out.println(String.valueOf(aMsg));
+	  }
 }
