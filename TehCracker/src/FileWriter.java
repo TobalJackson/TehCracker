e8ef5ba026b30701814f2e9c9ebf1e5a87dc83e4
Chris James <tobaljackson@gmail.com>
2013-10-24 12:59:13 -0400

    Generates arbitrary # of sit keys, but Integer.MAX_VALUE in hex only goes to 7fffffff!

--- TehCracker/src/FileWriter.java
+++ TehCracker/src/FileWriter.java
@@ -8,32 +8,45 @@ import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.StandardOpenOption;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 public class FileWriter {
 	public static void main(String[] args){
 		FileWriter fw = new FileWriter();
-		List<String> someLines = Arrays.asList("Hello thar", "You too!");
-		
+		List<String> sitKeys = new ArrayList<String>();
+		String sitPrefix = "SIT-";
 		try {
-			fw.writeTextFile("test", someLines);
+			for(int i = 0; i < 1000000; i++){
+				String key = Integer.toHexString(i);
+				StringBuilder keyPad = new StringBuilder(12);
+				keyPad.append(sitPrefix);
+				while((key.length() + keyPad.length()) < 12){
+					keyPad.append("0");
+				}
+				keyPad.append(key);
+				sitKeys.add(keyPad.toString());
+			}
+			fw.writeTextFile("sitkeys", sitKeys);
 			System.out.println("wrote some lines");
 		}
 		
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+		System.out.println(Integer.MAX_VALUE);
 	}
 	void writeTextFile(String fileName, List<String> linesToWrite) throws IOException{
 		Path path = Paths.get(fileName);
 		try {
 			BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
 			for(String line : linesToWrite){
-				System.out.println(line);
+				//System.out.println(line);
 				writer.write(line);
 				writer.newLine();
 			}
+			writer.close(); //adding close() wrote the lines.  I'm guessing that it won't write to the file until either the buffer is filled (8k?) or the stream is closed?
 		}
 		catch(IOException e){
 			e.printStackTrace();
